4637cac1915d20f7ed9c4dcca9e8484332e25f64
Konrad Malawski <konrad.malawski@project13.pl>
2018-02-01 14:53:26 +0900

    show how to terminate

--- src/main/scala/MyAkka.scala
+++ src/main/scala/MyAkka.scala
@@ -1,21 +1,16 @@
-import akka.actor.{Actor, ActorSystem, Props}
-import akka.stream.ActorMaterializer
-
-import akka.stream.alpakka.csv.scaladsl.CsvParsing
-import akka.stream.alpakka.csv.scaladsl.CsvToMap
 import java.nio.file.Paths
-import java.nio.charset.StandardCharsets
 
-import akka.NotUsed
-
-import akka.util.ByteString
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.alpakka.csv.scaladsl.{CsvParsing, CsvToMap}
 import akka.stream.scaladsl._
-import akka.stream.{ActorMaterializer}
+import akka.util.ByteString
 
 
 object Main extends App {
   implicit val system = ActorSystem("MyAkkaActor")
   implicit val materializer = ActorMaterializer()
+  implicit val dipatcher = system.dispatcher
 
   val path = Paths.get("test.csv")
   //val path = Paths.get("test_300000.csv")
@@ -27,16 +22,19 @@ object Main extends App {
   source
     .via(CsvParsing.lineScanner())
     .via(CsvToMap.withHeaders("firstname", "lastname", "gender", "birthday", "addr1", "addr2", "addr3", "state", "email", "zip", "tel", "attr", "regdate"))
-    .via(Flow[Map[String, ByteString]].map(_.map(r => (r._1, r._2.utf8String))))
-    .via(Flow[Map[String, String]].groupBy(2000, r => r(grp_col))
+    .map(_.map(r => (r._1, r._2.utf8String)))
+    .groupBy(2000, r => r(grp_col))
       .fold(acc_empty) { (acc: (String, Int), rec: Map[String, String]) =>
         val cnt = acc._2 + 1
         // println(rec(grp_col))
         (rec(grp_col), cnt)
       }.mergeSubstreams
-    )
-    .via(Flow[(String, Int)].map { case (groupname, count) => s"$groupname,$count" })
+    .map { case (groupname, count) => s"$groupname,$count" }
     .runWith(Sink.foreach(println))
+    .onComplete(done ⇒ {
+      println(done)
+      system.terminate()
+    })
     // .runForeach(println)
 
 }
