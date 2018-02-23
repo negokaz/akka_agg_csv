aa9d061c489592920091a9c5dd253947db89e858
Ueno Yoshihiro <enpedasi@gmail.com>
2018-02-23 11:33:56 +0900

    [Fix] Over 2000 lines shutdown problem.

--- src/main/scala/MyAkka.scala
+++ src/main/scala/MyAkka.scala
@@ -1,42 +1,52 @@
-import akka.actor.{Actor, ActorSystem, Props}
-import akka.stream.ActorMaterializer
-
-import akka.stream.alpakka.csv.scaladsl.CsvParsing
-import akka.stream.alpakka.csv.scaladsl.CsvToMap
 import java.nio.file.Paths
-import java.nio.charset.StandardCharsets
 
-import akka.NotUsed
-
-import akka.util.ByteString
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.alpakka.csv.scaladsl.{CsvParsing, CsvToMap}
 import akka.stream.scaladsl._
-import akka.stream.{ActorMaterializer}
+import akka.util.ByteString
 
 
 object Main extends App {
   implicit val system = ActorSystem("MyAkkaActor")
   implicit val materializer = ActorMaterializer()
+  implicit val dipatcher = system.dispatcher
 
-  val path = Paths.get("test.csv")
-  //val path = Paths.get("test_300000.csv")
-  val source = FileIO.fromPath(path)
 
-  val acc_empty = ("", 0)
-  val grp_col = "state"
+  // val path = if (params.isEmpty) "../fukuokaex/test_12_000_000.csv" else params(0)
+  //val path = "../fukuokaex/test_3_000_000.csv"
+  val path = "/home/enpedasi/fukuokaex/test_3_000_000.csv"
+  val source = FileIO.fromPath(Paths.get(path))
 
+  val acc_empty = Map.empty[String, Int]
+  var resultMap = Map.empty[String, Int]
+  val grp_col = "lastname"
+
+  val start = System.currentTimeMillis()
   source
     .via(CsvParsing.lineScanner())
     .via(CsvToMap.withHeaders("firstname", "lastname", "gender", "birthday", "addr1", "addr2", "addr3", "state", "email", "zip", "tel", "attr", "regdate"))
-    .via(Flow[Map[String, ByteString]].map(_.map(r => (r._1, r._2.utf8String))))
-    .via(Flow[Map[String, String]].groupBy(2000, r => r(grp_col))
-      .fold(acc_empty) { (acc: (String, Int), rec: Map[String, String]) =>
-        val cnt = acc._2 + 1
-        // println(rec(grp_col))
-        (rec(grp_col), cnt)
-      }.mergeSubstreams
-    )
-    .via(Flow[(String, Int)].map { case (groupname, count) => s"$groupname,$count" })
-    .runWith(Sink.foreach(println))
-    // .runForeach(println)
-
+    .map(_.map(r => (r._1, r._2.utf8String))) // Map( birthday ->'1950/01/20', email -> 'enpedasi@heaven.com' )
+    .filter(rec => rec.get(grp_col) != None)
+    .groupBy(30, r => r(grp_col)(0)) // 頭一文字でグループ分け
+    .async
+    .fold(acc_empty) { (acc: Map[String, Int], rec: Map[String, String]) =>
+      val word = rec(grp_col)
+      val cnt = acc.getOrElse(word, 0)
+      acc.updated(word, cnt + 1)
+    }
+    .mergeSubstreams
+    .via(Flow[Map[String, Int]].fold(acc_empty) { (acc: Map[String, Int], rec: Map[String, Int]) =>
+      acc ++ rec.map { case (k, v) => k -> (v + acc.getOrElse(k, 0)) }
+    })
+    .runWith(Sink.foreach(e => {
+      resultMap = e
+    }))
+    .onComplete(done ⇒ {
+      val msec = (System.currentTimeMillis - start)
+      println(done)
+      resultMap.toSeq.sortWith(_._2 > _._2).take(10).foreach(println)
+      println(msec + "msec")
+      system.terminate()
+    })
 }
