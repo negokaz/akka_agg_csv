fad1adf2c79f4272bcd557e71b187b274ce4e8b2
negokaz <negokaz@gmail.com>
2018-03-16 00:27:09 +0900

    use PartitionHub instead of groupby

--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ scalaVersion := "2.12.4"
 resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.5.9",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.11",
   "com.lightbend.akka" %% "akka-stream-alpakka-csv" % "0.13",
   "com.lightbend.akka" %% "akka-stream-alpakka-file" % "0.13",
   "org.apache.commons" % "commons-lang3" % "3.7"
--- src/main/scala/MyAkka.scala
+++ src/main/scala/MyAkka.scala
@@ -1,5 +1,5 @@
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings, OverflowStrategy}
 import akka.stream.scaladsl._
 import org.apache.commons.lang3.StringUtils
 
@@ -21,7 +21,7 @@ object Main extends App {
   val path = "./test_3_000_000.csv"
 
   /** 集計処理を何並列で処理するか */
-  val parallelism = 30
+  val parallelism = 2
 
   /** CSVの何列目に LastName があるか */
   val columnNumberOfLastName = 2
@@ -29,15 +29,23 @@ object Main extends App {
   val fileSource = scala.io.Source.fromFile(path, enc = "utf-8")
   val source = Source.fromIterator(() => fileSource.getLines())
 
+  val partitionedSource =
+    source
+      .map(rec => StringUtils.split(rec, ",", columnNumberOfLastName + 1)(columnNumberOfLastName - 1)) // String#split は正規表現を用いるため効率が悪い
+      .toMat(PartitionHub.sink(
+        partitioner = (size, rec) => Math.abs(rec.hashCode()) % size,
+        startAfterNrOfConsumers = parallelism))(Keep.right)
+
   val start = System.currentTimeMillis()
-  source
-    .map(rec => StringUtils.split(rec, ",", columnNumberOfLastName + 1)(columnNumberOfLastName - 1)) // String#split は正規表現を用いるため効率が悪い
-    .groupBy(parallelism, rec => Math.abs(rec.hashCode()) % parallelism) // String#hashCode を使って、名前ごとに一意の group にディスパッチされるようにする
-    .fold(AnyRefMap.empty[String, Int]) { (acc, rec: String) =>
-      // 効率が良い AnyRefMap を使う
-      acc.updated(rec, acc.getOrElse(rec, 0) + 1)
-    }
-    .mergeSubstreams
+  Source((1 to parallelism))
+    .flatMapConcat({ i =>
+      println(s"$i start")
+      partitionedSource.run()
+        .fold(AnyRefMap.empty[String, Int]) { (acc, rec: String) =>
+          // 効率が良い AnyRefMap を使う
+          acc.updated(rec, acc.getOrElse(rec, 0) + 1)
+        }
+    })
     .runWith(Sink.fold(AnyRefMap.empty[String, Int]) { (acc, rec: AnyRefMap[String, Int]) =>
       // LastName の hashCode でグループ分けしたため、acc と rec で同じキーの要素は存在しない
       acc ++ rec
@@ -49,7 +57,8 @@ object Main extends App {
         println(msec + "msec")
         fileSource.close()
         system.terminate()
-      case _ =>
+      case e =>
+        println(e)
         fileSource.close()
         system.terminate()
     }
